673c60f9662261a192f160de041619306e47f368
josuepdc <jospdc@gmail.com>
2020-09-23 21:28:58 -0300

    Implementando remoção de pratos de comida

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -69,7 +69,12 @@ const Dashboard: React.FC = () => {
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    try {
+      await api.delete(`foods/${id}`);
+      setFoods(foods.filter(x => x.id !== id));
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   function toggleModal(): void {
